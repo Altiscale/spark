4432eba20e14f1820e5a5467f0a71491c360d709
alee-altiscale <alee@altiscale.com>
2014-08-09 00:58:41 -0700

    [AE-727] Fix classpath issue

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -8,7 +8,7 @@ JAVA_HOME=/usr/java/default
 
 # This file is sourced when running various Spark programs.
 # Copy it as spark-env.sh and edit that to configure Spark for your site.
-SPARK_VERSION=1.1.0
+SPARK_VERSION="1.1.0-SNAPSHOT"
 
 # - SPARK_CLASSPATH, default classpath entries to append
 # Altiscale local libs and folders
