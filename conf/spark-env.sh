b1e9a2c7641168786a58a5f8e4a8cdee3be949ae
alee-altiscale <alee@altiscale.com>
2014-10-01 12:37:57 -0700

    official release 1.1.1 which is 1.1-rc1 for Spark. Remove SNAPSHOT

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -8,7 +8,7 @@ JAVA_HOME=/usr/java/default
 
 # This file is sourced when running various Spark programs.
 # Copy it as spark-env.sh and edit that to configure Spark for your site.
-SPARK_VERSION="1.1.1-SNAPSHOT"
+SPARK_VERSION="1.1.1"
 
 # - SPARK_CLASSPATH, default classpath entries to append
 # Altiscale local libs and folders
