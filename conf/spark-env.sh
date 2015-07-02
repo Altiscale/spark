664d89ecb4f4487eaeeac9a9c1de5b95e3edb37b
alee-altiscale <alee@altiscale.com>
2015-07-01 22:36:16 -0400

    [AE-1245] Fix spark-env.sh to honor env SPARK_VERSION when specified

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -8,7 +8,10 @@ JAVA_HOME=/usr/java/default
 
 # This file is sourced when running various Spark programs.
 # Copy it as spark-env.sh and edit that to configure Spark for your site.
-SPARK_VERSION="1.3.1"
+# User can override this SPARK_VERSION via env variable when necessary
+if [ "x${SPARK_VERSION}" = "x" ] ; then
+  SPARK_VERSION="1.3.1"
+fi
 
 # - SPARK_CLASSPATH, default classpath entries to append
 # Altiscale local libs and folders
