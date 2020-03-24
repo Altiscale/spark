e648bcfa00ae29568c86d2199bdb47e69ce0a41d
Andrew Lee <andrew.lee02@sap.com>
2020-03-24 12:14:54 -0700

    Bump Spark RPM file name from 2.3.2 to 2.3.4

--- cfg/export.ads
+++ cfg/export.ads
@@ -3,19 +3,19 @@ artifacts builderVersion: "1.1", {
   group "com.sap.bds.ats-altiscale", {
 
     artifact "spark", {
-      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.2.rpm"
+      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.4.rpm"
     }
 
     artifact "spark-shuffle", {
-      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.2-yarn-shuffle.rpm"
+      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.4-yarn-shuffle.rpm"
     }
 
     artifact "spark-devel", {
-      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.2-devel.rpm"
+      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.4-devel.rpm"
     }
 
     artifact "spark-kinesis", {
-      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.2-kinesis.rpm"
+      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.4-kinesis.rpm"
     }
   }
 }
