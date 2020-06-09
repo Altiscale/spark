893012b80c0255a6cec9d756bc40fb70da8fc622
Andrew Lee <andrew.lee02@sap.com>
2020-06-08 21:54:56 -0700

    Fix export filename

--- cfg/export.ads
+++ cfg/export.ads
@@ -3,7 +3,7 @@ artifacts builderVersion: "1.1", {
   group "com.sap.bds.ats-altiscale", {
 
     artifact "spark", {
-      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-2.3.2.rpm"
+      file "$gendir/src/spark_rpmbuild/rpm/alti-spark-${buildVersion}.noarch.rpm"
     }
 
     artifact "spark-shuffle", {
